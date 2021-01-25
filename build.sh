d577ab8ff756986ba4144b5b0b876f818d3bd849
Tom Lankhorst <info@tomlankhorst.nl>
2021-01-25 21:09:27 +0100

    Oops

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t magnum-build-$1 --build-arg FROM_IMG=ubuntu:$1 .
-docker run --rm -v./$1:/out magnum-build-bionic
+docker run --rm -v$(pwd)/$1:/out magnum-build-bionic
